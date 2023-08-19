f4fd91301df8a1092f135fa9d0c4eec672d469f9
Cameron Banowsky <cam@usa.dev>
2023-08-19 10:59:11 -0700

    update owasp ruleset
    Signed-off-by: Cameron Banowsky <cam@usa.dev>

--- self-managed/cloudflare/cloudflare.tf
+++ self-managed/cloudflare/cloudflare.tf
@@ -33,3 +33,34 @@ resource "cloudflare_record" "www" {
   type    = "A"
   proxied = true
 }
+
+resource "cloudflare_ruleset" "account_level_managed_waf" {
+  # Execute Cloudflare OWASP Core Ruleset
+  rules {
+    action = "execute"
+    action_parameters {
+      id = "4814384a9e5d4991b9815dcfc25d2f1f"
+      overrides {
+        # By default, all PL1 to PL4 rules are enabled.
+        # Set the paranoia level to PL2 by disabling rules with
+        # tags "paranoia-level-3" and "paranoia-level-4".
+        categories {
+          category = "paranoia-level-3"
+          status = "disabled"
+        }
+        categories {
+          category = "paranoia-level-4"
+          status = "disabled"
+        }
+        rules {
+          id = "6179ae15870a4bb7b2d480d4843b323c"
+          action = "log"
+          score_threshold = 60
+        }
+      }
+    }
+    expression = "true"
+    description = "zone"
+    enabled = true
+  }
+}
